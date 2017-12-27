31e39703c560459f57c63f36d7226183cb51b3b1
jeanmilost <jeanmilost@7e8401a7-7fe1-49ce-b830-781838938609>
2017-12-27 22:42:52 +0000

    - added: imported several functions in the collisions module from the miniAPI and DwarfStar Engine (NOT TERMINATED)

--- Engine/CSR_Collision.h
+++ Engine/CSR_Collision.h
@@ -27,7 +27,24 @@
 // Structures
 //---------------------------------------------------------------------------
 
-// Aligned-axis bounding box tree node prototype
+/**
+* Indexed polygon, keep the index of each vertex composing the polygon data inside a vertex buffer
+*/
+typedef struct
+{
+    size_t m_VertexIndex[3];
+} CSR_IndexedPolygon;
+
+/**
+* Indexed polygon array
+*/
+typedef struct
+{
+    CSR_IndexedPolygon* m_pBuffer;
+    size_t              m_Count;
+} CSR_IndexedPolygons;
+
+// Aligned-axis bounding box tree node prototype (required to use itself inside the structure)
 typedef struct CSR_AABBNode CSR_AABBNode;
 
 /**
@@ -35,12 +52,12 @@ typedef struct CSR_AABBNode CSR_AABBNode;
 */
 struct CSR_AABBNode
 {
-    CSR_AABBNode* m_pParent;
-    CSR_AABBNode* m_pLeft;
-    CSR_AABBNode* m_pRight;
-    CSR_Box*      m_pBox;
-    CSR_Polygon*  m_pPolygons;
-    size_t        m_PolygonsCount;
+    CSR_AABBNode*        m_pParent;
+    CSR_AABBNode*        m_pLeft;
+    CSR_AABBNode*        m_pRight;
+    CSR_Box*             m_pBox;
+    CSR_IndexedPolygons* m_pPolygons;
+    //REM? float*               m_pVertexBuffer;
 };
 
 #ifdef __cplusplus
@@ -49,124 +66,132 @@ struct CSR_AABBNode
 #endif
 
         //-------------------------------------------------------------------
-        // Aligned-Axis Bounding Box tree
+        // Indexed polygons
         //-------------------------------------------------------------------
 
         /**
-        * Extends a bounding box to include a polygon
-        *@param pP - polygon to add
-        *@param pB - bounding box in which polygon should be included
-        *@param empty - if true, box is empty an still no contains any polygon
+        * Converts an indexed polygon to a polygon
+        *@param pVB - vertex buffer containing the vertices referred by the indexed polygon
+        *@param pP - indexed polygon
+        *@param[out] pR - polygon
         */
-        void csrBoxExtendToPolygon(const CSR_Polygon* pP, CSR_Box* pB, int empty);
+        void csrIndexedToPolygon(const float* pVB, const CSR_IndexedPolygon* pP, CSR_Polygon3* pR);
 
         /**
-        * Extracts a polygon from a vertex buffer and adds it in an array
-        *@param pVB - source vertex buffer
-        *@param v1 - first polygon vertex index in vertex buffer
-        *@param v2 - second polygon vertex index in vertex buffer
-        *@param v3 - third polygon vertex index in vertex buffer
-        *@param[out] pPolygons - polygon array that contains generated polygons
-        *@param[out] pPolygonsCount - polygons count contained in array
-        *@note Generated polygons should be deleted by calling miniReleasePolygons()
-        *      when useless
+        * Adds a new indexed polygon, generated from his 3 indices, inside an indexed polygon buffer
+        *@param v1 - polygon first vertex index
+        *@param v2 - polygon second vertex index
+        *@param v3 - polygon third vertex index
+        *@param[out] pPolygons - indexed polygon buffer in which the new polygon should be added
+        *@note The buffer should be deleted by calling csrIndexedBufferRelease() when useless
         */
-        void csrPolygonFromVB(const float*        pVB,
-                                    unsigned      v1,
-                                    unsigned      v2,
-                                    unsigned      v3,
-                                    CSR_Polygon** pPolygons,
-                                    unsigned*     pPolygonsCount);
-
-        /**
-        * Extracts polygons from vertex buffer
-        *@param pVB - source vertex buffer
-        *@param length - vertex buffer length
-        *@param type - polygons type as arranged in vertex buffer, where:
-        *              0 = triangles
-        *              1 = triangle strip
-        *              2 = triangle fan
-        *              3 = quads
-        *              4 = quad strip
-        *@param[out] pPolygons - polygon array that contains generated polygons
-        *@param[out] pPolygonsCount - polygons count contained in array
-        *@return 1 on success, otherwise 0
-        *@note Generated polygons should be deleted by calling miniReleasePolygons()
-        *      when useless
-        */
-        int csrPolygonsFromVB(const float*        pVB,
-                                    unsigned      length,
-                                    unsigned      type,
-                                    unsigned      stride,
-                                    CSR_Polygon** pPolygons,
-                                    unsigned*     pPolygonsCount);
+        void csrIndexedBufferAdd(size_t v1, size_t v2, size_t v3, CSR_IndexedPolygons* pPolygons);
 
         /**
         * Releases polygons previously created by csrPolygonsFromVB() or csrPolygonFromVB()
         *@param pPolygons - polygon array to release
         */
-        void csrPolygonsRelease(CSR_Polygon* pPolygons);
-
-        /**
-        * Cuts box on the longest axis
-        *@param pBox - box to cut
-        *@param[out] pLeftBox - resulting left box
-        *@param[out] pRightBox - resulting right box
-        */
-        void csrBoxCut(const CSR_Box* pBox, CSR_Box* pLeftBox, CSR_Box* pRightBox);
-
-        /**
-        * Populates AABB tree
-        *@param pNode - root or parent node to create from
-        *@param pPolygons - source polygon array
-        *@param polygonsCount - polygon array count
-        *@return 1 on success, otherwise 0
-        */
-        int csrAABBTreePopulate(      CSR_AABBNode* pNode,
-                                const CSR_Polygon*  pPolygons,
-                                      size_t        polygonsCount);
-
-        /**
-        * Resolves AABB tree
-        *@param pRay - ray against which tree boxes will be tested
-        *@param pNode - root or parent node to resolve
-        *@param[out] pPolygons - polygons belonging to boxes hit by ray
-        *@param[out] polygonsCount - polygon array count
-        *@return 1 on success, otherwise 0
-        */
-        int csrAABBTreeResolve(CSR_Ray*      pRay,
-                               CSR_AABBNode* pNode,
-                               CSR_Polygon** pPolygons,
-                               unsigned*     pPolygonsCount);
-
-        /**
-        * Releases node content
-        *@param pNode - node for which content should be released
-        *@note Only the node content is released, the node itself is not released
-        */
-        void csrAABBTreeReleaseNode(CSR_AABBNode* pNode);
-
-        /**
-        * Releases tree content
-        *@param pNode - root node from which content should be released
-        */
-        void csrAABBTreeRelease(CSR_AABBNode* pNode);
+        void csrIndexedBufferRelease(CSR_IndexedPolygons* pPolygons);
 
         //-------------------------------------------------------------------
-        // Collision reaction
+        // Aligned-Axis Bounding Box tree
         //-------------------------------------------------------------------
-
-        /**
-        * Gets the next position while sliding on the plane
-        *@param pSlidingPlane - sliding plane
-        *@param pPosition - current position
-        *@param pRadius - radius around the current position
-        *@param pR - resulting position
-        */
-        void csrCollisionSlidingPos(const CSR_Plane*   pSlidingPlane,
-                                    const CSR_Vector3* pPosition,
-                                          float        radius,
-                                          CSR_Vector3* pR);
+//
+//        /**
+//        * Extends a bounding box to include a polygon
+//        *@param pVB - vertex buffer containing the indexed polygon vertices
+//        *@param pP - polygon to add
+//        *@param pB - bounding box in which polygon should be included
+//        *@param[in, out] pEmpty - if 1, box is empty and still no contains any polygon
+//        */
+//        void csrBoxExtendToPolygon(const float*              pVB,
+//                                   const CSR_IndexedPolygon* pP,
+//                                         CSR_Box*            pB,
+//                                         int*                pEmpty);
+//
+//        /**
+//        * Extracts polygons from vertex buffer
+//        *@param pVB - source vertex buffer
+//        *@param length - vertex buffer length
+//        *@param type - polygons type as arranged in vertex buffer, where:
+//        *              0 = triangles
+//        *              1 = triangle strip
+//        *              2 = triangle fan
+//        *              3 = quads
+//        *              4 = quad strip
+//        *@param[out] pPolygons - polygon array that contains generated polygons
+//        *@param[out] pPolygonsCount - polygons count contained in array
+//        *@return 1 on success, otherwise 0
+//        *@note Generated polygons should be deleted by calling miniReleasePolygons()
+//        *      when useless
+//        */
+//        int csrPolygonsFromVB(const float*        pVB,
+//                                    unsigned      length,
+//                                    unsigned      type,
+//                                    unsigned      stride,
+//                                    CSR_Polygon** pPolygons,
+//                                    unsigned*     pPolygonsCount);
+//
+//        /**
+//        * Cuts box on the longest axis
+//        *@param pBox - box to cut
+//        *@param[out] pLeftBox - resulting left box
+//        *@param[out] pRightBox - resulting right box
+//        */
+//        void csrBoxCut(const CSR_Box* pBox, CSR_Box* pLeftBox, CSR_Box* pRightBox);
+//
+//        /**
+//        * Populates AABB tree
+//        *@param pNode - root or parent node to create from
+//        *@param pPolygons - source polygon array
+//        *@param polygonsCount - polygon array count
+//        *@return 1 on success, otherwise 0
+//        */
+//        int csrAABBTreePopulate(      CSR_AABBNode* pNode,
+//                                const CSR_Polygon*  pPolygons,
+//                                      size_t        polygonsCount);
+//
+//        /**
+//        * Resolves AABB tree
+//        *@param pRay - ray against which tree boxes will be tested
+//        *@param pNode - root or parent node to resolve
+//        *@param[out] pPolygons - polygons belonging to boxes hit by ray
+//        *@param[out] polygonsCount - polygon array count
+//        *@return 1 on success, otherwise 0
+//        */
+//        int csrAABBTreeResolve(CSR_Ray*      pRay,
+//                               CSR_AABBNode* pNode,
+//                               CSR_Polygon** pPolygons,
+//                               unsigned*     pPolygonsCount);
+//
+//        /**
+//        * Releases node content
+//        *@param pNode - node for which content should be released
+//        *@note Only the node content is released, the node itself is not released
+//        */
+//        void csrAABBTreeReleaseNode(CSR_AABBNode* pNode);
+//
+//        /**
+//        * Releases tree content
+//        *@param pNode - root node from which content should be released
+//        */
+//        void csrAABBTreeRelease(CSR_AABBNode* pNode);
+//
+//        //-------------------------------------------------------------------
+//        // Collision reaction
+//        //-------------------------------------------------------------------
+//
+//        /**
+//        * Gets the next position while sliding on the plane
+//        *@param pSlidingPlane - sliding plane
+//        *@param pPosition - current position
+//        *@param pRadius - radius around the current position
+//        *@param pR - resulting position
+//        */
+//        void csrCollisionSlidingPos(const CSR_Plane*   pSlidingPlane,
+//                                    const CSR_Vector3* pPosition,
+//                                          float        radius,
+//                                          CSR_Vector3* pR);
 
 #ifdef __cplusplus
     }
