fd32297d51a99aee927aca7bf583bca0a67c7f0b
jeanmilost <jeanmilost@7e8401a7-7fe1-49ce-b830-781838938609>
2017-12-30 16:49:30 +0000

    - added: imported several functions in the models module from the miniAPI and DwarfStar Engine (NOT TERMINATED) - improved: made code security stronger - cleaned: code and comments

--- trunk/projects/CompactStar Engine/Engine/CSR_Models.c
+++ trunk/projects/CompactStar Engine/Engine/CSR_Models.c
@@ -17,6 +17,7 @@
 
 // std
 #include <stdlib.h>
+#include <string.h>
 
 //---------------------------------------------------------------------------
 // Vertex functions
@@ -58,6 +59,10 @@ void csrVertexBufferRelease(CSR_VertexBuffer* pVB)
 //---------------------------------------------------------------------------
 void csrVertexCalculateStride(CSR_VertexFormat* pVertexFormat)
 {
+    // no vertex format?
+    if (!pVertexFormat)
+        return;
+
     pVertexFormat->m_Stride = 3;
 
     // do use normals?
@@ -79,29 +84,64 @@ void csrVertexAdd(CSR_Vector3*      pVertex,
                   unsigned          color,
                   CSR_VertexBuffer* pVB)
 {
-    unsigned offset = pVB->m_Count;
+    float* pNewData;
+    size_t offset = pVB->m_Count;
 
-    // allocate memory for new vertex
+    // allocate memory for a new vertex
     if (!pVB->m_Count)
         pVB->m_pData = (float*)malloc(pVB->m_Format.m_Stride * sizeof(float));
     else
-        pVB->m_pData = (float*)realloc(pVB->m_pData,
-                                      (pVB->m_Count + pVB->m_Format.m_Stride) * sizeof(float));
+    {
+        pNewData = (float*)realloc(pVB->m_pData,
+                                  (pVB->m_Count + pVB->m_Format.m_Stride) * sizeof(float));
 
-    // copy vertex from source
-    pVB->m_pData[offset]     = pVertex->m_X;
-    pVB->m_pData[offset + 1] = pVertex->m_Y;
-    pVB->m_pData[offset + 2] = pVertex->m_Z;
+        // certify that the new memory block was well allocated
+        if (!pNewData)
+            return;
+
+        pVB->m_pData = pNewData;
+    }
+
+    // failed to allocate memory, do nothing
+    if (!pVB->m_pData)
+        return;
+
+    // source vertex exists?
+    if (!pVertex)
+    {
+        // cannot add a nonexistent vertex, fill with empty data in this case
+        pVB->m_pData[offset]     = 0.0f;
+        pVB->m_pData[offset + 1] = 0.0f;
+        pVB->m_pData[offset + 2] = 0.0f;
+    }
+    else
+    {
+        // copy vertex from source
+        pVB->m_pData[offset]     = pVertex->m_X;
+        pVB->m_pData[offset + 1] = pVertex->m_Y;
+        pVB->m_pData[offset + 2] = pVertex->m_Z;
+    }
 
     offset += 3;
 
     // do include normals?
     if (pVB->m_Format.m_UseNormals)
     {
-        // copy normal from source
-        pVB->m_pData[offset]     = pNormal->m_X;
-        pVB->m_pData[offset + 1] = pNormal->m_Y;
-        pVB->m_pData[offset + 2] = pNormal->m_Z;
+        // source normal exists?
+        if (!pNormal)
+        {
+            // cannot add a nonexistent normal, fill with empty data in this case
+            pVB->m_pData[offset]     = 0.0f;
+            pVB->m_pData[offset + 1] = 0.0f;
+            pVB->m_pData[offset + 2] = 0.0f;
+        }
+        else
+        {
+            // copy normal from source
+            pVB->m_pData[offset]     = pNormal->m_X;
+            pVB->m_pData[offset + 1] = pNormal->m_Y;
+            pVB->m_pData[offset + 2] = pNormal->m_Z;
+        }
 
         offset += 3;
     }
@@ -109,9 +149,19 @@ void csrVertexAdd(CSR_Vector3*      pVertex,
     // do include texture coordinates?
     if (pVB->m_Format.m_UseTextures)
     {
-        // copy texture coordinates from source
-        pVB->m_pData[offset]     = pUV->m_X;
-        pVB->m_pData[offset + 1] = pUV->m_Y;
+        // source texture coordinates exists?
+        if (!pUV)
+        {
+            // cannot add a nonexistent normal, fill with empty data in this case
+            pVB->m_pData[offset]     = 0.0f;
+            pVB->m_pData[offset + 1] = 0.0f;
+        }
+        else
+        {
+            // copy texture coordinates from source
+            pVB->m_pData[offset]     = pUV->m_X;
+            pVB->m_pData[offset + 1] = pUV->m_Y;
+        }
 
         offset += 2;
     }
@@ -152,12 +202,366 @@ void csrMeshRelease(CSR_Mesh* pMesh)
 {
     size_t i;
 
-    // release the mesh content
-    for (i = 0; i < pMesh->m_Count; ++i)
-        if (pMesh->m_pVB[i].m_pData)
-            free(pMesh->m_pVB[i].m_pData);
+    // no mesh to free?
+    if (!pMesh)
+        return;
+
+    // do free the mesh content?
+    if (pMesh->m_pVB)
+    {
+        // free the mesh vertex buffer content
+        for (i = 0; i < pMesh->m_Count; ++i)
+            if (pMesh->m_pVB[i].m_pData)
+                free(pMesh->m_pVB[i].m_pData);
+
+        // free the mesh vertex buffer
+        free(pMesh->m_pVB);
+    }
 
     // free the mesh
     free(pMesh);
+}
+//---------------------------------------------------------------------------
+// Face functions
+//---------------------------------------------------------------------------
+CSR_FaceBuffer* csrFaceBufferCreate(void)
+{
+    // create a new face buffer
+    CSR_FaceBuffer* pFB = (CSR_FaceBuffer*)malloc(sizeof(CSR_FaceBuffer));
+
+    // succeeded?
+    if (!pFB)
+        return 0;
+
+    // initialize the face buffer content
+    pFB->m_pData = 0;
+    pFB->m_Count = 0;
+
+    return pFB;
+}
+//---------------------------------------------------------------------------
+void csrFaceBufferRelease(CSR_FaceBuffer* pFB)
+{
+    size_t i;
+
+    // no face buffer to free?
+    if (!pFB)
+        return;
+
+    // do free the face buffer content?
+    if (pFB->m_pData)
+    {
+        // free the face buffer data content
+        for (i = 0; i < pFB->m_Count; ++i)
+            if (pFB->m_pData[i].m_pIndex)
+                free(pFB->m_pData[i].m_pIndex);
+
+        // free the face buffer data
+        free(pFB->m_pData);
+    }
+
+    // free the face buffer
+    free(pFB);
+}
+//---------------------------------------------------------------------------
+void csrFaceAdd(const size_t* pIndexes, size_t indexCount, CSR_FaceBuffer* pFB)
+{
+    size_t    offset;
+    CSR_Face* pNewFace;
+
+    // no source to add from?
+    if (!pIndexes || !indexCount)
+        return;
+
+    offset = pFB->m_Count;
+
+    // allocate memory for a new face
+    if (!pFB->m_Count)
+        pFB->m_pData = (CSR_Face*)malloc(sizeof(CSR_Face));
+    else
+    {
+        pNewFace = (CSR_Face*)realloc(pFB->m_pData, (pFB->m_Count + 1) * sizeof(CSR_Face));
+
+        // certify that the new memory block was well allocated
+        if (!pNewFace)
+            return;
+
+        pFB->m_pData = pNewFace;
+    }
+
+    // failed to allocate memory, do nothing
+    if (!pFB->m_pData)
+        return;
+
+    ++pFB->m_Count;
+
+    // allocate memory for the face data
+    pFB->m_pData[offset].m_pIndex = (size_t*)malloc(indexCount * sizeof(size_t));
+
+    // failed to allocate memory, do nothing
+    if (!pFB->m_pData[offset].m_pIndex)
+    {
+        pFB->m_pData[offset].m_Count = 0;
+        return;
+    }
+
+    // populate the face content
+    memcpy(pFB->m_pData[offset].m_pIndex, pIndexes, indexCount);
+    pFB->m_pData[offset].m_Count = indexCount;
+}
+//---------------------------------------------------------------------------
+CSR_FaceBuffer* csrFaceBufferFromVertexBuffer(const CSR_VertexBuffer* pVB)
+{
+    unsigned i;
+    unsigned index;
+    size_t   indexes[3];
+
+    // create a face buffer
+    CSR_FaceBuffer* pFB = csrFaceBufferCreate();
+
+    // succeeded?
+    if (!pFB)
+        return 0;
+
+    // assign the reference to the source vertex buffer
+    pFB->m_pVB = pVB;
+
+    // no data to extract from?
+    if (!pVB || !pVB->m_pData || !pVB->m_Count || !pVB->m_Format.m_Stride)
+        return pFB;
+
+    // search for vertex type
+    switch (!pVB->m_Format.m_Type)
+    {
+        case CSR_VT_Triangles:
+        {
+            // calculate iteration step
+            const unsigned step = (pVB->m_Format.m_Stride * 3);
+
+            // iterate through source vertices
+            for (i = 0; i < pVB->m_Count; i += step)
+            {
+                // extract polygon from source vertex buffer and add it to face buffer
+                indexes[0] = i;
+                indexes[1] = i +  pVB->m_Format.m_Stride;
+                indexes[2] = i + (pVB->m_Format.m_Stride * 2);
+                csrFaceAdd(indexes, 3, pFB);
+            }
+
+            return pFB;
+        }
+
+        case CSR_VT_TriangleStrip:
+        {
+            // calculate length to read in triangle strip buffer
+            const unsigned stripLength = (pVB->m_Count - (pVB->m_Format.m_Stride * 2));
+                           index       = 0;
+
+            // iterate through source vertices
+            for (i = 0; i < stripLength; i += pVB->m_Format.m_Stride)
+            {
+                // extract polygon from source buffer, revert odd polygons
+                if (!index || !(index % 2))
+                {
+                    indexes[0] = i;
+                    indexes[1] = i +  pVB->m_Format.m_Stride;
+                    indexes[2] = i + (pVB->m_Format.m_Stride * 2);
+                }
+                else
+                {
+                    indexes[0] = i +  pVB->m_Format.m_Stride;
+                    indexes[1] = i;
+                    indexes[2] = i + (pVB->m_Format.m_Stride * 2);
+                }
+
+                csrFaceAdd(indexes, 3, pFB);
+                ++index;
+            }
+
+            return pFB;
+        }
+
+        case CSR_VT_TriangleFan:
+        {
+            // calculate length to read in triangle fan buffer
+            const unsigned fanLength = (pVB->m_Count - pVB->m_Format.m_Stride);
+
+            // iterate through source vertices
+            for (i = pVB->m_Format.m_Stride; i < fanLength; i += pVB->m_Format.m_Stride)
+            {
+                // extract polygon from source buffer
+                indexes[0] = 0;
+                indexes[1] = i;
+                indexes[2] = i + pVB->m_Format.m_Stride;
+                csrFaceAdd(indexes, 3, pFB);
+            }
+
+            return pFB;
+        }
+
+        case CSR_VT_Quads:
+        {
+            // calculate iteration step
+            const unsigned step = (pVB->m_Format.m_Stride * 4);
+
+            // iterate through source vertices
+            for (i = 0; i < pVB->m_Count; i += step)
+            {
+                // calculate vertices position
+                const unsigned v1 = i;
+                const unsigned v2 = i +  pVB->m_Format.m_Stride;
+                const unsigned v3 = i + (pVB->m_Format.m_Stride * 2);
+                const unsigned v4 = i + (pVB->m_Format.m_Stride * 3);
+
+                // extract first polygon from source buffer
+                indexes[0] = v1;
+                indexes[1] = v2;
+                indexes[2] = v3;
+                csrFaceAdd(indexes, 3, pFB);
+
+                // extract second polygon from source buffer
+                indexes[0] = v3;
+                indexes[1] = v2;
+                indexes[2] = v4;
+                csrFaceAdd(indexes, 3, pFB);
+            }
+
+            return pFB;
+        }
+
+        case CSR_VT_QuadStrip:
+        {
+            // calculate iteration step
+            const unsigned step = (pVB->m_Format.m_Stride * 2);
+
+            // calculate length to read in triangle strip buffer
+            const unsigned stripLength = (pVB->m_Count - (pVB->m_Format.m_Stride * 2));
+
+            // iterate through source vertices
+            for (i = 0; i < stripLength; i += step)
+            {
+                // calculate vertices position
+                const unsigned v1 = i;
+                const unsigned v2 = i +  pVB->m_Format.m_Stride;
+                const unsigned v3 = i + (pVB->m_Format.m_Stride * 2);
+                const unsigned v4 = i + (pVB->m_Format.m_Stride * 3);
+
+                // extract first polygon from source buffer
+                indexes[0] = v1;
+                indexes[1] = v2;
+                indexes[2] = v3;
+                csrFaceAdd(indexes, 3, pFB);
+
+                // extract second polygon from source buffer
+                indexes[0] = v3;
+                indexes[1] = v2;
+                indexes[2] = v4;
+                csrFaceAdd(indexes, 3, pFB);
+            }
+
+            return pFB;
+        }
+
+        default:
+            return pFB;
+    }
+}
+//---------------------------------------------------------------------------
+// Shape functions
+//---------------------------------------------------------------------------
+CSR_Mesh* csrShapeCreateSurface(const CSR_VertexFormat* pVertexFormat,
+                                      float             width,
+                                      float             height,
+                                      unsigned          color)
+{
+    int         i;
+    int         index;
+    CSR_Mesh*   pMesh;
+    CSR_Vector3 vertex;
+    CSR_Vector3 normal;
+    CSR_Vector2 uv;
+
+    // create a buffer template: 0 for negative values, 1 for positive
+    const int bufferTemplate[] =
+    {
+        0, 0,
+        0, 1,
+        1, 0,
+        1, 1,
+    };
+
+    // no vertex format?
+    if (!pVertexFormat)
+        return 0;
+
+    // create a mesh to contain the shape
+    pMesh = csrMeshCreate();
+
+    // succeeded?
+    if (!pMesh)
+        return 0;
+
+    // create a vertex buffer
+    pMesh->m_pVB = csrVertexBufferCreate();
+
+    // succeeded?
+    if (!pMesh->m_pVB)
+    {
+        csrMeshRelease(pMesh);
+        return 0;
+    }
+
+    // configure the vertex format
+    pMesh->m_pVB[0].m_Format = *pVertexFormat;
+    csrVertexCalculateStride(&pMesh->m_pVB[0].m_Format);
+
+    // iterate through vertex to create
+    for (i = 0; i < 4; ++i)
+    {
+        // calculate template buffer index
+        index = i * 2;
+
+        // populate vertex buffer
+        if (bufferTemplate[index])
+            vertex.m_X =  width / 2.0f;
+        else
+            vertex.m_X = -width / 2.0f;
+
+        if (bufferTemplate[index + 1])
+            vertex.m_Y =  height / 2.0f;
+        else
+            vertex.m_Y = -height / 2.0f;
+
+        vertex.m_Z = 0.0f;
+
+        // do include normals?
+        if (pMesh->m_pVB[0].m_Format.m_UseNormals)
+        {
+            normal.m_X =  0.0f;
+            normal.m_Y =  0.0f;
+            normal.m_Z = -1.0f;
+        }
+
+        // do include texture?
+        if (pMesh->m_pVB[0].m_Format.m_UseTextures)
+        {
+            // calculate texture u coordinate
+            if (bufferTemplate[index])
+                uv.m_X = 1.0f;
+            else
+                uv.m_X = 0.0f;
+
+            // calculate texture v coordinate
+            if (bufferTemplate[index + 1])
+                uv.m_Y = 1.0f;
+            else
+                uv.m_Y = 0.0f;
+        }
+
+        // add the vertex to the buffer
+        csrVertexAdd(&vertex, &normal, &uv, color, pMesh->m_pVB);
+    }
+
+    return pMesh;
 }
 //---------------------------------------------------------------------------
--- trunk/projects/CompactStar Engine/Engine/CSR_Models.h
+++ trunk/projects/CompactStar Engine/Engine/CSR_Models.h
@@ -66,6 +66,25 @@ typedef struct
     size_t            m_Count;
 } CSR_Mesh;
 
+/**
+* Face, it's a list of indexes pointing to the vertices composing a face inside a vertex buffer
+*/
+typedef struct
+{
+    size_t* m_pIndex;
+    size_t  m_Count;
+} CSR_Face;
+
+/**
+* Face buffer
+*/
+typedef struct
+{
+    const CSR_VertexBuffer* m_pVB;
+    CSR_Face*               m_pData;
+    size_t                  m_Count;
+} CSR_FaceBuffer;
+
 #ifdef __cplusplus
     extern "C"
     {
@@ -123,6 +142,178 @@ typedef struct
         *@param pMesh - mesh to release
         */
         void csrMeshRelease(CSR_Mesh* pMesh);
+
+        //-------------------------------------------------------------------
+        // Face functions
+        //-------------------------------------------------------------------
+
+        /**
+        * Creates a face buffer
+        *@return newly created face buffer, 0 on error
+        *@note The face buffer must be released when no longer used, see csrFaceBufferRelease()
+        */
+        CSR_FaceBuffer* csrFaceBufferCreate(void);
+
+        /**
+        * Releases a face buffer
+        *@param pFB - face buffer to release
+        */
+        void csrFaceBufferRelease(CSR_FaceBuffer* pFB);
+
+        /**
+        * Adds a face to a face buffer
+        *@param pIndexes - index list composing the face to add to face buffer
+        *@param indexCount - index count
+        *@param[in, out] pFB - face buffer to add to
+        */
+        void csrFaceAdd(const size_t* pIndexes, size_t indexCount, CSR_FaceBuffer* pFB);
+
+        /**
+        * Gets polygons from vertex buffer
+        *@param pVB - source vertex buffer
+        *@return newly created face buffer, 0 on error
+        *@note The face buffer must be released when no longer used, see csrFaceBufferRelease()
+        *@note BE CAREFUL, the face buffer is valid as long as his source vertex buffer is valid. If
+        *      the vertex buffer is released, the face buffer should be released together
+        */
+        CSR_FaceBuffer* csrFaceBufferFromVertexBuffer(const CSR_VertexBuffer* pVB);
+
+        //-------------------------------------------------------------------
+        // Shape functions
+        //-------------------------------------------------------------------
+
+        /**
+        * Creates a surface
+        *@param width - surface width
+        *@param height - surface height
+        *@param color - color in ARGB format
+        *@return mesh containing the surface, 0 on error
+        *@note The mesh must be released when no longer used, see csrMeshRelease()
+        */
+        CSR_Mesh* csrShapeCreateSurface(const CSR_VertexFormat* pVertexFormat,
+                                              float             width,
+                                              float             height,
+                                              unsigned          color);
+
+//        /**
+//        * Creates a sphere
+//        *@param pRadius - sphere radius
+//        *@param slices - slices (longitude) number
+//        *@param stacks - stacks (latitude) number
+//        *@param color - color in RGBA format
+//        *@param [in, out] pVertexFormat - vertex format to use
+//        *@param[out] pVertices - resulting vertex buffer
+//        *@param[out] pVertexCount - vertex count in buffer
+//        *@param[out] pIndexes - index list containing offset and length of each vertex buffer composing sphere
+//        *@param[out] pIndexCount - resulting index count
+//        */
+//        void miniCreateSphere(const float*             pRadius,
+//                                    int                slices,
+//                                    int                stacks,
+//                                    unsigned           color,
+//                                    MINI_VertexFormat* pVertexFormat,
+//                                    float**            pVertices,
+//                                    unsigned*          pVertexCount,
+//                                    MINI_Index**       pIndexes,
+//                                    unsigned*          pIndexCount);
+//
+//        /**
+//        * Creates a cylinder
+//        *@param pRadius - cylinder radius
+//        *@param pHeight - cylinder height
+//        *@param faces - number of faces composing the cylinder
+//        *@param color - color in RGBA format
+//        *@param[in, out] pVertexFormat - vertex format to use
+//        *@param[out] pVertices - resulting vertex buffer
+//        *@param[out] pVertexCount - vertex count in buffer
+//        */
+//        void miniCreateCylinder(const float*             pRadius,
+//                                const float*             pHeight,
+//                                      int                faces,
+//                                      unsigned           color,
+//                                      MINI_VertexFormat* pVertexFormat,
+//                                      float**            pVertices,
+//                                      unsigned*          pVertexCount);
+//
+//        /**
+//        * Creates a disk
+//        *@param centerX - the disk center on the x axis
+//        *@param centerY - the disk center on the y axis
+//        *@param radius - disk radius
+//        *@param sliceCount - disk slice count
+//        *@param color - disk color
+//        *@param pVertexFormat - vertex format to apply
+//        *@param[out] pVertices - resulting vertex buffer
+//        *@param[out] pVertexCount - vertex count in buffer
+//        */
+//        void miniCreateDisk(float              centerX,
+//                            float              centerY,
+//                            float              radius,
+//                            unsigned           sliceCount,
+//                            unsigned           color,
+//                            MINI_VertexFormat* pVertexFormat,
+//                            float**            pVertices,
+//                            unsigned*          pVertexCount);
+//
+//        /**
+//        * Creates a ring
+//        *@param centerX - the ring center on the x axis
+//        *@param centerY - the ring center on the y axis
+//        *@param minRadius - internal radius
+//        *@param maxRadius - external radius
+//        *@param sliceCount - slice count
+//        *@param minColor - internal edge color
+//        *@param maxColor - external edge color
+//        *@param pVertexFormat - vertex format to apply
+//        *@param[out] pVertices - resulting vertex buffer
+//        *@param[out] pVertexCount - vertex count in buffer
+//        */
+//        void miniCreateRing(float              centerX,
+//                            float              centerY,
+//                            float              minRadius,
+//                            float              maxRadius,
+//                            unsigned           sliceCount,
+//                            unsigned           minColor,
+//                            unsigned           maxColor,
+//                            MINI_VertexFormat* pVertexFormat,
+//                            float**            pVertices,
+//                            unsigned*          pVertexCount);
+//
+//        /**
+//        * Creates a spiral
+//        *@param centerX - the spiral center on the x axis
+//        *@param centerY - the spiral center on the y axis
+//        *@param minRadius - internal radius
+//        *@param maxRadius - external radius
+//        *@param deltaMin - delta to apply to the min radius
+//        *@param deltaMax - delta to apply to the max radius
+//        *@param deltaZ - delta to apply to the z radius
+//        *@param stackCount - stack count
+//        *@param sliceCount - slice count
+//        *@param minColor - internal edge color
+//        *@param maxColor - external edge color
+//        *@param pVertexFormat - vertex format to apply
+//        *@param[out] pVertices - resulting vertex buffer
+//        *@param[out] pVertexCount - vertex count in buffer
+//        *@param[out] pIndexes - resulting index buffer
+//        *@param[out] pIndexCount - index count in buffer
+//        */
+//        void miniCreateSpiral(float              centerX,
+//                              float              centerY,
+//                              float              minRadius,
+//                              float              maxRadius,
+//                              float              deltaMin,
+//                              float              deltaMax,
+//                              float              deltaZ,
+//                              unsigned           stackCount,
+//                              unsigned           sliceCount,
+//                              unsigned           minColor,
+//                              unsigned           maxColor,
+//                              MINI_VertexFormat* pVertexFormat,
+//                              float**            pVertices,
+//                              unsigned*          pVertexCount,
+//                              MINI_Index**       pIndexes,
+//                              unsigned*          pIndexCount);
 
 #ifdef __cplusplus
     }
