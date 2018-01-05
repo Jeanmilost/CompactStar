6534835224fa91173e64a5ef6e4d1bf83c69cf4b
jeanmilost <jeanmilost@7e8401a7-7fe1-49ce-b830-781838938609>
2018-01-05 03:10:28 +0000

    - added: sphere model - changed: created a small demo to show a sphere (NOT TERMINATED)

--- trunk/projects/CompactStar Engine/Level designer/Main.h
+++ trunk/projects/CompactStar Engine/Level designer/Main.h
@@ -1,26 +1,90 @@
-//---------------------------------------------------------------------------
-
 #ifndef MainH
 #define MainH
-//---------------------------------------------------------------------------
+
 #include <System.Classes.hpp>
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
 #include <Vcl.ComCtrls.hpp>
 #include <Vcl.ExtCtrls.hpp>
-//---------------------------------------------------------------------------
+
+// compactStar engine
+#include "CSR_Model.h"
+#include "CSR_Shader.h"
+
 class TMainForm : public TForm
 {
-__published:	// IDE-managed Components
-    TPageControl *pcMain;
-    TTabSheet *tsView;
-    TPanel *paView;
-private:	// User declarations
-public:		// User declarations
-    __fastcall TMainForm(TComponent* Owner);
+    __published:
+        TPageControl *pcMain;
+        TTabSheet *tsView;
+        TPanel *paView;
+
+        void __fastcall FormShow(TObject *Sender);
+        void __fastcall FormResize(TObject *Sender);
+
+    public:
+        __fastcall TMainForm(TComponent* pOwner);
+        virtual __fastcall ~TMainForm();
+
+    private:
+        HDC              m_hDC;
+        HGLRC            m_hRC;
+        CSR_Shader*      m_pShader;
+        CSR_Mesh*        m_pSphere;
+        unsigned __int64 m_PreviousTime;
+
+        /**
+        * Enables OpenGL
+        *@param hWnd - Windows handle
+        *@param hDC - device context
+        *@param hRC - OpenGL rendering context
+        */
+        void EnableOpenGL(HWND hWnd, HDC* hDC, HGLRC* hRC);
+
+        /**
+        * Disables OpenGL
+        *@param hWnd - Windows handle
+        *@param hDC - device context
+        *@param hRC - OpenGL rendering context
+        */
+        void DisableOpenGL(HWND hWnd, HDC hDC, HGLRC hRC);
+
+        /**
+        * Creates the viewport
+        *@param w - viewport width
+        *@param h - viewport height
+        */
+        void CreateViewport(float w, float h);
+
+        /**
+        * Initializes the scene
+        *@param w - scene width
+        *@param h - scene height
+        */
+        void InitScene(int w, int h);
+
+        /**
+        * Deletes the scene
+        */
+        void DeleteScene();
+
+        /**
+        * Updates the scene
+        *@param elapsedTime - elapsed time since last update, in milliseconds
+        */
+        void UpdateScene(float elapsedTime);
+
+        /**
+        * Draws the scene
+        */
+        void DrawScene();
+
+        /**
+        * Called while application is idle
+        *@param pSender - event sender
+        *@param[in, out] done - if true, event is done and will no longer be called
+        */
+        void __fastcall OnIdle(TObject* pSender, bool& done);
 };
-//---------------------------------------------------------------------------
-extern PACKAGE TMainForm *MainForm;
-//---------------------------------------------------------------------------
+extern PACKAGE TMainForm* MainForm;
 #endif
