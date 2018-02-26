6eef32f0263b09deee6a8d6473e1582dc923d7fb
jeanmilost <jeanmilost@7e8401a7-7fe1-49ce-b830-781838938609>
2018-02-26 19:52:48 +0000

    - changed: first animated scene in software rasterizer project

--- trunk/projects/CompactStar Engine/Tools/Software raster/Main.cpp
+++ trunk/projects/CompactStar Engine/Tools/Software raster/Main.cpp
@@ -15,83 +15,126 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 
-CSR_PixelBuffer* g_pModelTexture   = 0;
-
 //---------------------------------------------------------------------------
 TMainForm *MainForm;
 //---------------------------------------------------------------------------
-__fastcall TMainForm::TMainForm(TComponent* Owner)
-    : TForm(Owner)
+__fastcall TMainForm::TMainForm(TComponent* pOwner) :
+    TForm(pOwner),
+    m_pModel(NULL),
+    m_pModelTexture(NULL),
+    m_pFrameBuffer(NULL),
+    m_pDepthBuffer(NULL),
+    m_zNear(1.0f),
+    m_zFar(1000.0f),
+    m_pTextureLastTime(0.0),
+    m_pModelLastTime(0.0),
+    m_pMeshLastTime(0.0),
+    m_TextureIndex(0),
+    m_ModelIndex(0),
+    m_MeshIndex(0),
+    m_StartTime(0),
+    m_PreviousTime(0),
+    m_Initialized(false)
 {}
+//---------------------------------------------------------------------------
+__fastcall TMainForm::~TMainForm()
+{
+    DeleteScene();
+}
+//---------------------------------------------------------------------------
+void __fastcall TMainForm::FormShow(TObject *Sender)
+{
+    // initialize the scene
+    InitScene(ClientWidth, ClientHeight);
+
+    // initialize the timer
+    m_StartTime    = ::GetTickCount();
+    m_PreviousTime = ::GetTickCount();
+
+    // listen the application idle
+    Application->OnIdle = OnIdle;
+}
 //------------------------------------------------------------------------------
-void OnTextureRead(std::size_t index, const CSR_PixelBuffer* pPixelBuffer)
+void TMainForm::TextureRead(std::size_t index, const CSR_PixelBuffer* pPixelBuffer)
 {
-    // no pixel buffer?
-    if (!pPixelBuffer)
-        return;
-
-    // release the previously existing texture, if any
-    if (g_pModelTexture)
-        csrPixelBufferRelease(g_pModelTexture);
-
-    // copy the pixel buffer content (the source buffer will be released sooner)
-    g_pModelTexture = (CSR_PixelBuffer*)malloc(sizeof(CSR_PixelBuffer));
-    memcpy(g_pModelTexture, pPixelBuffer, sizeof(CSR_PixelBuffer));
-
-    // copy the texture pixel data
-    g_pModelTexture->m_pData = (unsigned char*)malloc(pPixelBuffer->m_DataLength);
-    memcpy(g_pModelTexture->m_pData, pPixelBuffer->m_pData, pPixelBuffer->m_DataLength);
+    static_cast<TMainForm*>(Application->MainForm)->OnTextureRead(index, pPixelBuffer);
 }
 //---------------------------------------------------------------------------
-void TMainForm::OnApplyFragmentShader(const CSR_Matrix4*  pMatrix,
-                                      const CSR_Polygon3* pPolygon,
-                                      const CSR_Vector2*  pST,
-                                      const CSR_Vector3*  pSampler,
-                                            float         z,
-                                            CSR_Color*    pColor)
+void TMainForm::ApplyFragmentShader(const CSR_Matrix4*  pMatrix,
+                                    const CSR_Polygon3* pPolygon,
+                                    const CSR_Vector2*  pST,
+                                    const CSR_Vector3*  pSampler,
+                                          float         z,
+                                          CSR_Color*    pColor)
 {
-    float  stX;
-    float  stY;
-    size_t x;
-    size_t y;
-    size_t line;
-
-    // limit the texture coordinate between 0 and 1 (equivalent to OpenGL clamp mode)
-    csrMathClamp(pST->m_X, 0.0f, 1.0f, &stX);
-    csrMathClamp(pST->m_Y, 0.0f, 1.0f, &stY);
-
-    // calculate the x and y coordinate to pick in the texture, and the line length in pixels
-    x    = stX * g_pModelTexture->m_Width;
-    y    = stY * g_pModelTexture->m_Height;
-    line = g_pModelTexture->m_Width * g_pModelTexture->m_BytePerPixel;
-
-    // calculate the pixel index to get
-    const size_t index = (y * line) + (x * g_pModelTexture->m_BytePerPixel);
-
-    // get the pixel color from texture
-    pColor->m_R = (float)(((unsigned char*)(g_pModelTexture->m_pData))[index])     / 255.0f;
-    pColor->m_G = (float)(((unsigned char*)(g_pModelTexture->m_pData))[index + 1]) / 255.0f;
-    pColor->m_B = (float)(((unsigned char*)(g_pModelTexture->m_pData))[index + 2]) / 255.0f;
-    pColor->m_A = 1.0f;
+    static_cast<TMainForm*>(Application->MainForm)->OnApplyFragmentShader(pMatrix,
+                                                                        pPolygon,
+                                                                        pST,
+                                                                        pSampler,
+                                                                        z,
+                                                                        pColor);
 }
-//---------------------------------------------------------------------------
-void __fastcall TMainForm::bt1Click(TObject *Sender)
+//------------------------------------------------------------------------------
+void TMainForm::InitScene(int w, int h)
 {
-    const uint32_t imageWidth  = 640;
-    const uint32_t imageHeight = 480;
-    const float    zNear       = 1.0f;
-    const float    zFar        = 1000.0f;
-
-    CSR_Matrix4 projectionMatrix;
-    float w = imageWidth;
-    float h = imageHeight;
+    // initialize the raster
+    csrRasterInit(&m_Raster);
 
     // calculate matrix items
     const float fov    = 45.0f;
-    const float aspect = w / h;
+    const float aspect = float(w) / float(h);
 
-    csrMat4Perspective(fov, aspect, zNear, zFar, &projectionMatrix);
+    csrMat4Perspective(fov, aspect, m_zNear, m_zFar, &m_ProjectionMatrix);
 
+    // create the frame and depth buffers
+    m_pFrameBuffer = csrFrameBufferCreate(w, h);
+    m_pDepthBuffer = csrDepthBufferCreate(w, h);
+
+    // configure the MDL model vertex format
+    CSR_VertexFormat vf;
+    vf.m_HasNormal         = 0;
+    vf.m_HasTexCoords      = 1;
+    vf.m_HasPerVertexColor = 0;
+
+    CSR_VertexCulling vc;
+    vc.m_Type = CSR_CT_Back;
+    vc.m_Face = CSR_CF_CCW;
+
+    // load MDL model
+    m_pModel = csrMDLOpen("N:\\Jeanmilost\\Devel\\Projects\\CompactStar Engine\\Common\\Models\\wizard.mdl",
+                          0,
+                          &vf,
+                          &vc,
+                          0,
+                          0,
+                          TextureRead);
+
+    m_Initialized = true;
+}
+//------------------------------------------------------------------------------
+void TMainForm::DeleteScene()
+{
+    m_Initialized = false;
+
+    // release the model texture
+    if (m_pModelTexture)
+        csrPixelBufferRelease(m_pModelTexture);
+
+    // release the model
+    if (m_pModel)
+        csrMDLRelease(m_pModel);
+
+    // release the depth buffer
+    if (m_pDepthBuffer)
+        csrDepthBufferRelease(m_pDepthBuffer);
+
+    // release the frame buffer
+    if (m_pFrameBuffer)
+        csrFrameBufferRelease(m_pFrameBuffer);
+}
+//---------------------------------------------------------------------------
+void TMainForm::UpdateScene(float elapsedTime)
+{
     float       angle;
     CSR_Vector3 t;
     CSR_Vector3 axis;
@@ -104,12 +147,7 @@ void __fastcall TMainForm::bt1Click(TObject *Sender)
     CSR_Matrix4 combinedMatrixLevel2;
     CSR_Matrix4 modelViewMatrix;
     CSR_Matrix4 worldToCamera;
-    /*
-    worldToCamera.m_Table[0][0] =  0.707107f; worldToCamera.m_Table[0][1] = -0.331295f; worldToCamera.m_Table[0][2] =  0.624695f;  worldToCamera.m_Table[0][3] = 0.0f;
-    worldToCamera.m_Table[1][0] =  0.0f;      worldToCamera.m_Table[1][1] =  0.883452f; worldToCamera.m_Table[1][2] =  0.468521f;  worldToCamera.m_Table[1][3] = 0.0f;
-    worldToCamera.m_Table[2][0] = -0.707107f; worldToCamera.m_Table[2][1] = -0.331295f; worldToCamera.m_Table[2][2] =  0.624695f;  worldToCamera.m_Table[2][3] = 0.0f;
-    worldToCamera.m_Table[3][0] = -1.63871f;  worldToCamera.m_Table[3][1] = -5.747777f; worldToCamera.m_Table[3][2] = -40.400412f; worldToCamera.m_Table[3][3] = 1.0f;
-    */
+
     // set translation
     t.m_X =  0.0f;
     t.m_Y =  0.0f;
@@ -133,7 +171,7 @@ void __fastcall TMainForm::bt1Click(TObject *Sender)
     axis.m_Z = 0.0f;
 
     // set rotation angle
-    angle = M_PI * 0.25;
+    angle = -M_PI * 0.25;
 
     csrMat4Rotate(angle, &axis, &rotateMatrixY);
 
@@ -150,137 +188,152 @@ void __fastcall TMainForm::bt1Click(TObject *Sender)
     csrMat4Multiply(&combinedMatrixLevel2, &translateMatrix, &modelViewMatrix);
 
     // build the final matrix
-    csrMat4Multiply(&modelViewMatrix, &projectionMatrix, &worldToCamera);
+    csrMat4Multiply(&modelViewMatrix, &m_ProjectionMatrix, &m_Matrix);
 
-    const uint32_t ntris = 3156;
-
+    // calculate the next indexes to use for the MDL model
+    csrMDLUpdateIndex(m_pModel,
+                      10,
+                      0,
+                     &m_TextureIndex,
+                     &m_ModelIndex,
+                     &m_MeshIndex,
+                     &m_pTextureLastTime,
+                     &m_pModelLastTime,
+                     &m_pMeshLastTime,
+                      elapsedTime);
+}
+//------------------------------------------------------------------------------
+void TMainForm::DrawScene()
+{
     CSR_Pixel pixel;
     pixel.m_R = 0;
     pixel.m_G = 0;
     pixel.m_B = 0;
     pixel.m_A = 255;
 
-    // create the frame buffer
-    CSR_FrameBuffer* pFrameBuffer = csrFrameBufferCreate(imageWidth, imageHeight);
-    csrFrameBufferClear(pFrameBuffer, &pixel);
+    // clear the buffers
+    csrFrameBufferClear(m_pFrameBuffer, &pixel);
+    csrDepthBufferClear(m_pDepthBuffer, m_zFar);
 
-    // create the depth buffer
-    CSR_DepthBuffer* pDepthBuffer = csrDepthBufferCreate(imageWidth, imageHeight);
-    csrDepthBufferClear(pDepthBuffer, zFar);
+    // get the current model mesh to draw
+    const CSR_Mesh* pMesh = csrMDLGetMesh(m_pModel, m_ModelIndex, m_MeshIndex);
 
-    CSR_VertexBuffer vb;
+    // found it?
+    if (!pMesh)
+        return;
 
-    CSR_Vector3 vertices[1732];
-    CSR_Vector2 st[3056];
-
-    for (std::size_t i = 0; i < 1732; ++i)
-    {
-        vertices[i].m_X = verticesRAW[i * 3];
-        vertices[i].m_Y = verticesRAW[(i * 3) + 1];
-        vertices[i].m_Z = verticesRAW[(i * 3) + 2];
-    }
-
-    for (std::size_t i = 0; i < 3056; ++i)
-    {
-        st[i].m_X = stRAW[i * 2];
-        st[i].m_Y = stRAW[(i * 2) + 1];
-    }
-
-    vb.m_Format.m_Type              = CSR_VT_Triangles;
-    vb.m_Format.m_HasNormal         = 0;
-    vb.m_Format.m_HasTexCoords      = 1;
-    vb.m_Format.m_HasPerVertexColor = 0;
-    vb.m_Format.m_Stride            = 5;
-    vb.m_Culling.m_Type             = CSR_CT_Back;
-    vb.m_Culling.m_Face             = CSR_CF_CW;
-    vb.m_Count                      = ntris * 3 * vb.m_Format.m_Stride;
-    vb.m_pData                      = (float*)malloc(vb.m_Count * sizeof(float));
-
-    for (std::size_t i = 0; i < ntris; ++i)
-    {
-        vb.m_pData[ i * 3 * 5]      = vertices[nvertices[i * 3]].m_X;
-        vb.m_pData[(i * 3 * 5) + 1] = vertices[nvertices[i * 3]].m_Y;
-        vb.m_pData[(i * 3 * 5) + 2] = vertices[nvertices[i * 3]].m_Z;
-        vb.m_pData[(i * 3 * 5) + 3] = st[stindices[i * 3]].m_X;
-        vb.m_pData[(i * 3 * 5) + 4] = st[stindices[i * 3]].m_Y;
-
-        vb.m_pData[(i * 3 * 5) + 5] = vertices[nvertices[i * 3 + 1]].m_X;
-        vb.m_pData[(i * 3 * 5) + 6] = vertices[nvertices[i * 3 + 1]].m_Y;
-        vb.m_pData[(i * 3 * 5) + 7] = vertices[nvertices[i * 3 + 1]].m_Z;
-        vb.m_pData[(i * 3 * 5) + 8] = st[stindices[i * 3 + 1]].m_X;
-        vb.m_pData[(i * 3 * 5) + 9] = st[stindices[i * 3 + 1]].m_Y;
-
-        vb.m_pData[(i * 3 * 5) + 10] = vertices[nvertices[i * 3 + 2]].m_X;
-        vb.m_pData[(i * 3 * 5) + 11] = vertices[nvertices[i * 3 + 2]].m_Y;
-        vb.m_pData[(i * 3 * 5) + 12] = vertices[nvertices[i * 3 + 2]].m_Z;
-        vb.m_pData[(i * 3 * 5) + 13] = st[stindices[i * 3 + 2]].m_X;
-        vb.m_pData[(i * 3 * 5) + 14] = st[stindices[i * 3 + 2]].m_Y;
-    }
-
-    // configure the MDL model vertex format
-    CSR_VertexFormat vf;
-    vf.m_HasNormal         = 0;
-    vf.m_HasTexCoords      = 1;
-    vf.m_HasPerVertexColor = 0;
-
-    CSR_VertexCulling vc;
-    vc.m_Type = CSR_CT_Back;
-    vc.m_Face = CSR_CF_CW;
-
-    // load MDL model
-    CSR_MDL* pMDL = csrMDLOpen("N:\\Jeanmilost\\Devel\\Projects\\CompactStar Engine\\Common\\Models\\wizard.mdl",
-                               0,
-                              &vf,
-                              &vc,
-                               0,
-                               0,
-                               OnTextureRead);
-
-    CSR_Raster raster;
-    csrRasterInit(&raster);
-
-    csrRasterDraw(&worldToCamera,
-                   zNear,
-                   zFar,
-                   pMDL->m_pModel[0].m_pMesh[0].m_pVB,// &vb,
-                  &raster,
-                   pFrameBuffer,
-                   pDepthBuffer,
+    // draw the model
+    csrRasterDraw(&m_Matrix,
+                   m_zNear,
+                   m_zFar,
+                   pMesh->m_pVB,
+                  &m_Raster,
+                   m_pFrameBuffer,
+                   m_pDepthBuffer,
                    0,
-                   OnApplyFragmentShader);
+                   ApplyFragmentShader);
 
     std::auto_ptr<TBitmap> pBitmap(new TBitmap());
     pBitmap->PixelFormat = pf24bit;
-    pBitmap->SetSize(imageWidth, imageHeight);
+    pBitmap->SetSize(ClientWidth, ClientHeight);
 
-    for (std::size_t y = 0; y < imageHeight; ++y)
+    for (std::size_t y = 0; y < ClientHeight; ++y)
     {
         TRGBTriple* pLine = reinterpret_cast<TRGBTriple*>(pBitmap->ScanLine[y]);
 
-        for (std::size_t x = 0; x < imageWidth; ++x)
+        for (std::size_t x = 0; x < ClientWidth; ++x)
         {
-            pLine[x].rgbtRed   = pFrameBuffer->m_pPixel[y * imageWidth + x].m_R;
-            pLine[x].rgbtGreen = pFrameBuffer->m_pPixel[y * imageWidth + x].m_G;
-            pLine[x].rgbtBlue  = pFrameBuffer->m_pPixel[y * imageWidth + x].m_B;
+            pLine[x].rgbtRed   = m_pFrameBuffer->m_pPixel[y * ClientWidth + x].m_R;
+            pLine[x].rgbtGreen = m_pFrameBuffer->m_pPixel[y * ClientWidth + x].m_G;
+            pLine[x].rgbtBlue  = m_pFrameBuffer->m_pPixel[y * ClientWidth + x].m_B;
         }
     }
 
-    if (pMDL)
-        csrMDLRelease(pMDL);
-
-    free(vb.m_pData);
-
-    // release the depth buffer
-    csrDepthBufferRelease(pDepthBuffer);
-
-    // release the frame buffer
-    csrFrameBufferRelease(pFrameBuffer);
-
-    //REM delete [] frameBuffer;
-    //REM delete [] depthBuffer;
-
-    //pBitmap->SaveToFile(L"test.bmp");
-    //REM im1->Picture->Assign(pBitmap.get());
     Canvas->Draw(0, 0, pBitmap.get());
+}
+//---------------------------------------------------------------------------
+void TMainForm::OnDrawScene(bool resize)
+{
+    // do draw the scene for a resize?
+    if (resize)
+    {
+        if (!m_Initialized)
+            return;
+
+        // just process a minimal draw
+        UpdateScene(0.0);
+        DrawScene();
+
+        return;
+    }
+
+    // calculate time interval
+    const unsigned __int64 now            = ::GetTickCount();
+    const double           elapsedTime    = (now - m_PreviousTime) / 1000.0;
+                           m_PreviousTime =  now;
+
+    if (!m_Initialized)
+        return;
+
+    // update and draw the scene
+    UpdateScene(elapsedTime);
+    DrawScene();
+}
+//------------------------------------------------------------------------------
+void TMainForm::OnTextureRead(std::size_t index, const CSR_PixelBuffer* pPixelBuffer)
+{
+    // no pixel buffer?
+    if (!pPixelBuffer)
+        return;
+
+    // release the previously existing texture, if any
+    if (m_pModelTexture)
+        csrPixelBufferRelease(m_pModelTexture);
+
+    // copy the pixel buffer content (the source buffer will be released sooner)
+    m_pModelTexture = (CSR_PixelBuffer*)malloc(sizeof(CSR_PixelBuffer));
+    memcpy(m_pModelTexture, pPixelBuffer, sizeof(CSR_PixelBuffer));
+
+    // copy the texture pixel data
+    m_pModelTexture->m_pData = (unsigned char*)malloc(pPixelBuffer->m_DataLength);
+    memcpy(m_pModelTexture->m_pData, pPixelBuffer->m_pData, pPixelBuffer->m_DataLength);
+}
+//---------------------------------------------------------------------------
+void TMainForm::OnApplyFragmentShader(const CSR_Matrix4*  pMatrix,
+                                      const CSR_Polygon3* pPolygon,
+                                      const CSR_Vector2*  pST,
+                                      const CSR_Vector3*  pSampler,
+                                            float         z,
+                                            CSR_Color*    pColor)
+{
+    float  stX;
+    float  stY;
+    size_t x;
+    size_t y;
+    size_t line;
+
+    // limit the texture coordinate between 0 and 1 (equivalent to OpenGL clamp mode)
+    csrMathClamp(pST->m_X, 0.0f, 1.0f, &stX);
+    csrMathClamp(pST->m_Y, 0.0f, 1.0f, &stY);
+
+    // calculate the x and y coordinate to pick in the texture, and the line length in pixels
+    x    = stX * m_pModelTexture->m_Width;
+    y    = stY * m_pModelTexture->m_Height;
+    line = m_pModelTexture->m_Width * m_pModelTexture->m_BytePerPixel;
+
+    // calculate the pixel index to get
+    const size_t index = (y * line) + (x * m_pModelTexture->m_BytePerPixel);
+
+    // get the pixel color from texture
+    pColor->m_R = (float)(((unsigned char*)(m_pModelTexture->m_pData))[index])     / 255.0f;
+    pColor->m_G = (float)(((unsigned char*)(m_pModelTexture->m_pData))[index + 1]) / 255.0f;
+    pColor->m_B = (float)(((unsigned char*)(m_pModelTexture->m_pData))[index + 2]) / 255.0f;
+    pColor->m_A = 1.0f;
+}
+//---------------------------------------------------------------------------
+void __fastcall TMainForm::OnIdle(TObject* pSender, bool& done)
+{
+    done = false;
+    OnDrawScene(false);
 }
 //---------------------------------------------------------------------------
--- trunk/projects/CompactStar Engine/Tools/Software raster/Main.h
+++ trunk/projects/CompactStar Engine/Tools/Software raster/Main.h
@@ -15,20 +15,86 @@
 class TMainForm : public TForm
 {
     __published:
-        TButton *bt1;
-
-        void __fastcall bt1Click(TObject *Sender);
+    void __fastcall FormShow(TObject *Sender);
 
     public:
-        __fastcall TMainForm(TComponent* Owner);
+        __fastcall TMainForm(TComponent* pOwner);
+
+        virtual __fastcall ~TMainForm();
 
     private:
-        static void OnApplyFragmentShader(const CSR_Matrix4*  pMatrix,
-                                          const CSR_Polygon3* pPolygon,
-                                          const CSR_Vector2*  pST,
-                                          const CSR_Vector3*  pSampler,
-                                                float         z,
-                                                CSR_Color*    pColor);
+        CSR_Raster       m_Raster;
+        CSR_Matrix4      m_ProjectionMatrix;
+        CSR_Matrix4      m_Matrix;
+        CSR_MDL*         m_pModel;
+        CSR_PixelBuffer* m_pModelTexture;
+        CSR_FrameBuffer* m_pFrameBuffer;
+        CSR_DepthBuffer* m_pDepthBuffer;
+        float            m_zNear;
+        float            m_zFar;
+        double           m_pTextureLastTime;
+        double           m_pModelLastTime;
+        double           m_pMeshLastTime;
+        std::size_t      m_TextureIndex;
+        std::size_t      m_ModelIndex;
+        std::size_t      m_MeshIndex;
+        unsigned __int64 m_StartTime;
+        unsigned __int64 m_PreviousTime;
+        bool             m_Initialized;
+
+        /**
+        * Initializes the scene
+        *@param w - scene width
+        *@param h - scene height
+        */
+        void InitScene(int w, int h);
+
+        /**
+        * Deletes the scene
+        */
+        void DeleteScene();
+
+        /**
+        * Updates the scene
+        *@param elapsedTime - elapsed time since last update, in milliseconds
+        */
+        void UpdateScene(float elapsedTime);
+
+        /**
+        * Draws the scene
+        */
+        void DrawScene();
+
+        static void TextureRead(std::size_t index, const CSR_PixelBuffer* pPixelBuffer);
+
+        static void ApplyFragmentShader(const CSR_Matrix4*  pMatrix,
+                                        const CSR_Polygon3* pPolygon,
+                                        const CSR_Vector2*  pST,
+                                        const CSR_Vector3*  pSampler,
+                                              float         z,
+                                              CSR_Color*    pColor);
+
+        void OnTextureRead(std::size_t index, const CSR_PixelBuffer* pPixelBuffer);
+
+        void OnApplyFragmentShader(const CSR_Matrix4*  pMatrix,
+                                   const CSR_Polygon3* pPolygon,
+                                   const CSR_Vector2*  pST,
+                                   const CSR_Vector3*  pSampler,
+                                         float         z,
+                                         CSR_Color*    pColor);
+
+        /**
+        * Called when the scene should be drawn
+        *@param resize - if true, the scene should be repainted during a resize
+        */
+        void OnDrawScene(bool resize);
+
+        /**
+        * Called while application is idle
+        *@param pSender - event sender
+        *@param[in, out] done - if true, event is done and will no longer be called
+        */
+        void __fastcall OnIdle(TObject* pSender, bool& done);
 };
 extern PACKAGE TMainForm *MainForm;
 #endif
