8156f471cc705985df7c735da541b09dcecbfa5f
jeanmilost <jeanmilost@7e8401a7-7fe1-49ce-b830-781838938609>
2018-03-12 19:06:52 +0000

    - added: scene collection (NOT TERMINATED) - added: csrSceneDrawVertexBuffer() function (code was previously embedded in csrSceneDrawMesh() function) - changed: moved code in CSR_Scene to CSR_Renderer, and deleted useless CSR_Scene module (this code is in fact part of the renderer)

--- trunk/projects/CompactStar Engine/SDK/CSR_Renderer.h
+++ trunk/projects/CompactStar Engine/SDK/CSR_Renderer.h
@@ -18,8 +18,23 @@
 
 // compactStar engine
 #include "CSR_Common.h"
+#include "CSR_Collision.h"
 #include "CSR_Model.h"
 #include "CSR_Shader.h"
+
+//---------------------------------------------------------------------------
+// Enumerators
+//---------------------------------------------------------------------------
+
+/**
+* Scene item type
+*/
+typedef enum
+{
+    CSR_IT_Mesh,
+    CSR_IT_Model,
+    CSR_IT_MDL
+} CSR_ESceneItemType;
 
 //---------------------------------------------------------------------------
 // Structures
@@ -42,6 +57,56 @@
         size_t            m_Factor;
     } CSR_MSAA;
 #endif
+
+/**
+* Scene collection item
+*/
+typedef struct
+{
+    void*              m_pItem;
+    CSR_ESceneItemType m_Type;
+    CSR_AABBNode       m_AABBTree;
+} CSR_SceneCollectionItem;
+
+/**
+* Scene collection, contains the models drawn in the scene and pointed by the scene object
+*/
+typedef struct
+{
+    CSR_SceneCollectionItem* m_pItem;
+} CSR_SceneCollection;
+
+/**
+* Scene item
+*/
+typedef struct
+{
+    void*              m_pItem;
+    CSR_ESceneItemType m_Type;
+    CSR_Matrix4        m_Matrix;
+    CSR_Shader*        m_pShader;
+    size_t             m_TextureIndex;
+    size_t             m_ModelIndex;
+    size_t             m_MeshIndex;
+    int                m_Visible;
+} CSR_SceneItem;
+
+/**
+* Scene
+*/
+typedef struct
+{
+    CSR_Color      m_Color;
+    CSR_Matrix4    m_Matrix;
+    CSR_SceneItem* m_pItem;
+    size_t         m_ItemCount;
+    CSR_SceneItem* m_pTransparentItem;
+    size_t         m_TransparentItemCount;
+
+    #ifndef CSR_OPENGL_2_ONLY
+        CSR_MSAA*  m_pMSAA;
+    #endif
+} CSR_Scene;
 
 #ifdef __cplusplus
     extern "C"
@@ -112,8 +177,93 @@
         #endif
 
         //-------------------------------------------------------------------
+        // Scene item functions
+        //-------------------------------------------------------------------
+
+        /**
+        * Creates a scene item
+        *@return newly created scene item, 0 on error
+        *@note The scene item must be released when no longer used, see csrSceneItemRelease()
+        */
+        CSR_SceneItem* csrSceneItemCreate(void);
+
+        /**
+        * Releases a scene item
+        *@param[in, out] pSceneItem - scene item to release
+        */
+        void csrSceneItemRelease(CSR_SceneItem* pSceneItem);
+
+        /**
+        * Initializes a scene item structure
+        *@param[in, out] pSceneItem - scene item to initialize
+        */
+        void csrSceneItemInit(CSR_SceneItem* pSceneItem);
+
+        /**
+        * Draws a scene item
+        *@param pScene - scene at which the item belongs
+        *@param pSceneItem - scene item to draw
+        */
+        void csrSceneItemDraw(const CSR_Scene* pScene, const CSR_SceneItem* pSceneItem);
+
+        //-------------------------------------------------------------------
         // Scene functions
         //-------------------------------------------------------------------
+
+        /**
+        * Creates a scene
+        *@return newly created scene, 0 on error
+        *@note The scene must be released when no longer used, see csrSceneRelease()
+        */
+        CSR_Scene* csrSceneCreate(void);
+
+        /**
+        * Releases a scene
+        *@param[in, out] pScene - scene to release
+        */
+        void csrSceneRelease(CSR_Scene* pScene);
+
+        /**
+        * Initializes a scene structure
+        *@param[in, out] pScene - scene to initialize
+        */
+        void csrSceneInit(CSR_Scene* pScene);
+
+        /**
+        * Adds a mesh to a scene
+        *@param pScene - scene in which the mesh will be added
+        *@param pMesh - mesh to add
+        *@param pShader - shader to use to draw the mesh
+        *@param transparent - if 1, the mesh is transparent, if 0 the mesh is opaque
+        *@return 1 on success, otherwise 0
+        *@note Because the scene will keep a reference on the added mesh and shader, these objects
+        *      should be released only after the scene is released
+        */
+        int csrSceneAddMesh(CSR_Scene* pScene, CSR_Mesh* pMesh, CSR_Shader* pShader, int transparent);
+
+        /**
+        * Adds a model to a scene
+        *@param pScene - scene in which the model will be added
+        *@param pModel- model to add
+        *@param pShader - shader to use to draw the model
+        *@param transparent - if 1, the model is transparent, if 0 the model is opaque
+        *@return 1 on success, otherwise 0
+        *@note Because the scene will keep a reference on the added mesh and shader, these objects
+        *      should be released only after the scene is released
+        */
+        int csrSceneAddModel(CSR_Scene* pScene, CSR_Model* pModel, CSR_Shader* pShader, int transparent);
+
+        /**
+        * Adds a MDL model to a scene
+        *@param pScene - scene in which the model will be added
+        *@param pMDL - model to add
+        *@param pShader - shader to use to draw the model
+        *@param transparent - if 1, the model is transparent, if 0 the model is opaque
+        *@return 1 on success, otherwise 0
+        *@note Because the scene will keep a reference on the added mesh and shader, these objects
+        *      should be released only after the scene is released
+        */
+        int csrSceneAddMDL(CSR_Scene* pScene, CSR_MDL* pMDL, CSR_Shader* pShader, int transparent);
 
         /**
         * Begins to draw a scene
@@ -125,6 +275,14 @@
         * Ends to draw a scene
         */
         void csrSceneEnd(void);
+
+        /**
+        * Draws a vertex buffer in a scene
+        *@param pVB - vertex buffer to draw
+        *@param pShader - shader to use to draw the vertex buffer
+        *@note The shader must be first enabled with the csrShaderEnable() function
+        */
+        void csrSceneDrawVertexBuffer(const CSR_VertexBuffer* pVB, CSR_Shader* pShader);
 
         /**
         * Draws a mesh in a scene
@@ -154,6 +312,12 @@
                                    size_t      textureIndex,
                                    size_t      modelIndex,
                                    size_t      meshIndex);
+
+        /**
+        * Draws a scene
+        *@param pScene - scene to draw
+        */
+        void csrSceneDraw(const CSR_Scene* pScene);
 
 #ifdef __cplusplus
     }
