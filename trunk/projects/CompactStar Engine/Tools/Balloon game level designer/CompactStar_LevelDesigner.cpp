80f0220d1782da512559f4fec6bf01a7ca7d6ce0
jeanmilost <jeanmilost@7e8401a7-7fe1-49ce-b830-781838938609>
2018-06-04 19:45:53 +0000

    - added: level designer project for a future balloon game

---
+++ trunk/projects/CompactStar Engine/Tools/Balloon game level designer/CompactStar_LevelDesigner.cpp
@@ -0,0 +1,59 @@
+/****************************************************************************
+ * ==> CompactStar_GroundCollision -----------------------------------------*
+ ****************************************************************************
+ * Description : Ground collision tool main entry point                     *
+ * Developer   : Jean-Milost Reymond                                        *
+ * Copyright   : 2017 - 2018, this file is part of the CompactStar Engine.  *
+ *               You are free to copy or redistribute this file, modify it, *
+ *               or use it for your own projects, commercial or not. This   *
+ *               file is provided "as is", WITHOUT ANY WARRANTY OF ANY      *
+ *               KIND. THE DEVELOPER IS NOT RESPONSIBLE FOR ANY DAMAGE OF   *
+ *               ANY KIND, ANY LOSS OF DATA, OR ANY LOSS OF PRODUCTIVITY    *
+ *               TIME THAT MAY RESULT FROM THE USAGE OF THIS SOURCE CODE,   *
+ *               DIRECTLY OR NOT.                                           *
+ ****************************************************************************/
+
+#include <vcl.h>
+#pragma hdrstop
+#pragma package(smart_init) // madExcept
+#pragma link "madExcept"
+#pragma link "madLinkDisAsm"
+#pragma link "madListHardware"
+#pragma link "madListProcesses"
+#pragma link "madListModules"
+
+// vcl
+#include <tchar.h>
+
+//---------------------------------------------------------------------------
+USEFORM("Main.cpp", MainForm);
+USEFORM("TLandscapeSelection.cpp", LandscapeSelection);
+//---------------------------------------------------------------------------
+int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+    try
+    {
+        Application->Initialize();
+        Application->MainFormOnTaskBar = true;
+        Application->CreateForm(__classid(TMainForm), &MainForm);
+         Application->Run();
+    }
+    catch (Exception &exception)
+    {
+        Application->ShowException(&exception);
+    }
+    catch (...)
+    {
+        try
+        {
+            throw Exception("");
+        }
+        catch (Exception &exception)
+        {
+            Application->ShowException(&exception);
+        }
+    }
+
+    return 0;
+}
+//---------------------------------------------------------------------------
