53257db5846566a92b2783d24cb508384706cf89
jeanmilost <jeanmilost@7e8401a7-7fe1-49ce-b830-781838938609>
2018-06-23 17:31:06 +0000

    - added: missing new CSR_Serializer class

---
+++ Tools/Level designer 2D/Classes/Serializer/CSR_SceneSerializer.cpp
@@ -0,0 +1,25 @@
+/****************************************************************************
+ * ==> CSR_SceneSerializer -------------------------------------------------*
+ ****************************************************************************
+ * Description : This module provides the functions required to serialize   *
+ *               the scene                                                  *
+ * Developer   : Jean-Milost Reymond                                        *
+ * Copyright   : 2017 - 2018, this file is part of the CompactStar Engine.  *
+ *               You are free to copy or redistribute this file, modify it, *
+ *               or use it for your own projects, commercial or not. This   *
+ *               file is provided "as is", WITHOUT ANY WARRANTY OF ANY      *
+ *               KIND. THE DEVELOPER IS NOT RESPONSIBLE FOR ANY DAMAGE OF   *
+ *               ANY KIND, ANY LOSS OF DATA, OR ANY LOSS OF PRODUCTIVITY    *
+ *               TIME THAT MAY RESULT FROM THE USAGE OF THIS SOURCE CODE,   *
+ *               DIRECTLY OR NOT.                                           *
+ ****************************************************************************/
+
+#include "CSR_SceneSerializer.h"
+
+// xml
+#include "sxmlc.h"
+
+//---------------------------------------------------------------------------
+// CSR_SceneSerializer
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
