43c66fe0b3653a394ac2376cc4f559a7f9d2f1f2
Jean-Milost Reymond <jean_milost@hotmail.com>
2022-03-24 14:34:28 -0300

    - added: Inter-Quake Model (.iqm) demo for xCode - changed: updated all dates to 2022 - cleaned: code and comments

---
+++ Demos/iOS/Metal/IQM Demo/Classes/GameViewController.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+ * ==> Animated Inter-Quake model demo -------------------------------------*
+ ****************************************************************************
+ * Description : A bone based animation using Inter-Quake (.iqm) model      *
+ * Developer   : Jean-Milost Reymond                                        *
+ * Copyright   : 2017 - 2022, this file is part of the CompactStar Engine.  *
+ *               You are free to copy or redistribute this file, modify it, *
+ *               or use it for your own projects, commercial or not. This   *
+ *               file is provided "as is", WITHOUT ANY WARRANTY OF ANY      *
+ *               KIND. THE DEVELOPER IS NOT RESPONSIBLE FOR ANY DAMAGE OF   *
+ *               ANY KIND, ANY LOSS OF DATA, OR ANY LOSS OF PRODUCTIVITY    *
+ *               TIME THAT MAY RESULT FROM THE USAGE OF THIS SOURCE CODE,   *
+ *               DIRECTLY OR NOT.                                           *
+ ****************************************************************************/
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+
+// compactStar engine
+#include "CSR_Texture.h"
+#include "CSR_Scene.h"
+
+/**
+* Game view controller, contains the application main view and his associated events
+*@author Jean-Milost Reymond
+*/
+@interface GameViewController : UIViewController
+{}
+
+@end
